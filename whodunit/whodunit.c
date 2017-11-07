60d65fad13b69a9af3dc6fe42844cfaa078e93ee
Rebecca Wagaman <ribexy@gmail.com>
2017-11-07 23:20:16 +0000

    Continue scaffolding with comments

--- whodunit/whodunit.c
+++ whodunit/whodunit.c
@@ -1,9 +1,20 @@
+// decode the secret image by altering red pixels
+
 #include <stdio.h>
+#include <stdlib.h>
 
-
-
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
+
+// two command line arguments: name of input file, name of output file
+
+// if args != 2, fprintf to stderr a usage message, return 1
+
+// if the file cannot be opened, notify user and return 2
+
+// if output file cannot be opened, notify user and return 3
+
+// if input is not 24bit uncompressed BMP 4.0, notify user and return 4
 
 // open file
 
@@ -15,6 +26,6 @@ int main(int argc, char *argv)
 
 // write verdict's scanline, pixel by pixel
 
-
+// return 0 upon success
 
 }
