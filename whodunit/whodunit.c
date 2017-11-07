dad96dc57eddede0dcfb491bbb0009cc1b658db7
Rebecca Wagaman <ribexy@gmail.com>
2017-11-07 23:12:40 +0000

    Skeleton for whodunit

---
+++ whodunit/whodunit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv)
+{
+
+// open file
+
+// update header's info for outfile
+
+// read clue's scanline, pixel by pixel
+
+// change pixel's color as necessary
+
+// write verdict's scanline, pixel by pixel
+
+
+
+}
